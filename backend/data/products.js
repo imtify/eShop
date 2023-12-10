88f9fd7e5fcd6a6de614977e53dfcc9f238c6473
imtify <imti.bh@gmail.com>
2023-12-10 15:30:35 +0600

    Mongodb and Mongoose integration completed for project

---
+++ backend/data/products.js
@@ -0,0 +1,81 @@
+const products = [
+  {
+    _id: "1",
+    name: "Apple MacBook Pro 2023",
+    image: "/images/airpods.jpg",
+    description:
+      "The Apple MacBook Pro 2023 is a powerhouse built for professionals. Featuring the latest M2 chip, stunning Retina display, and exceptional battery life, it offers seamless performance for demanding tasks.",
+    brand: "Apple",
+    category: "Laptop",
+    price: 2399.99,
+    countInStock: 30,
+    rating: 4.9,
+    numReviews: 120,
+  },
+  {
+    _id: "2",
+    name: "Dell XPS 15",
+    image: "/images/alexa.jpg",
+    description:
+      "The Dell XPS 15 boasts an InfinityEdge display, powerful Intel Core i9 processor, and NVIDIA GeForce RTX graphics. Its sleek design and outstanding performance make it a favorite among professionals and content creators.",
+    brand: "Dell",
+    category: "Laptop",
+    price: 1999.99,
+    countInStock: 25,
+    rating: 4.8,
+    numReviews: 95,
+  },
+  {
+    _id: "3",
+    name: "HP Spectre x360",
+    image: "/images/camera.jpg",
+    description:
+      "The HP Spectre x360 is a versatile 2-in-1 laptop with a stunning 4K OLED display, powerful Intel Core i7 processor, and exceptional battery life. Its convertible design offers flexibility for work and entertainment.",
+    brand: "HP",
+    category: "Laptop",
+    price: 1799.99,
+    countInStock: 28,
+    rating: 4.7,
+    numReviews: 85,
+  },
+  {
+    _id: "4",
+    name: "Lenovo ThinkPad X1 Carbon",
+    image: "/images/mouse.jpg",
+    description:
+      "The Lenovo ThinkPad X1 Carbon is a premium business laptop known for its durability and performance. With a lightweight design, powerful Intel Core i7 processor, and exceptional keyboard, it's the go-to choice for professionals.",
+    brand: "Lenovo",
+    category: "Laptop",
+    price: 1699.99,
+    countInStock: 20,
+    rating: 4.6,
+    numReviews: 75,
+  },
+  {
+    _id: "5",
+    name: "Asus ROG Zephyrus G14",
+    image: "/images/phone.jpg",
+    description:
+      "The Asus ROG Zephyrus G14 is a gaming powerhouse packed into a compact form. Featuring an AMD Ryzen 9 processor and NVIDIA GeForce RTX graphics, it delivers exceptional gaming performance on-the-go.",
+    brand: "Asus",
+    category: "Laptop",
+    price: 1499.99,
+    countInStock: 22,
+    rating: 4.8,
+    numReviews: 90,
+  },
+  {
+    _id: "6",
+    name: "Microsoft Surface Laptop Studio",
+    image: "/images/playstation.jpg",
+    description:
+      "The Microsoft Surface Laptop Studio offers a versatile design with a unique 2-in-1 form factor. With a powerful Intel Core i7 processor and stunning PixelSense display, it's perfect for creators and professionals.",
+    brand: "Microsoft",
+    category: "Laptop",
+    price: 2099.99,
+    countInStock: 26,
+    rating: 4.9,
+    numReviews: 110,
+  },
+];
+export default products;
