0f5f29cc5f5d23541d8aa0f5164022dd72353717
imtify <imti.bh@gmail.com>
2024-03-09 19:02:53 +0600

    Change filename logic for upload file

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -121,7 +121,7 @@ const createProductReview = asyncHandler(async (req, res) => {
 
 //Get top product GET/api/products/top in private
 const getTopProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+  const products = await Product.find({}).sort({ rating: -1 }).limit(5);
   res.json(products);
 });
 
