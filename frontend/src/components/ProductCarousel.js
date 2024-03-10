e4b95a571d88d09ebc21330ba4cc8b412c51c0c9
imtify <imti.bh@gmail.com>
2024-03-10 15:23:15 +0600

    Carousel Link to Comming soon text

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,18 +1,38 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Carousel, Image } from "react-bootstrap";
+import { Carousel, Image, Modal } from "react-bootstrap";
 import campaign from "../Campaign";
 
 const ProductCarousel = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = () => setShowModal(true);
+
   return (
-    <Carousel pause="hover" className="bg-primary mb-4">
-      {campaign?.map((c) => (
-        <Carousel.Item key={c.id}>
-          <Link to="/" className="d-flex align-items-center">
-            <Image src={c.image} alt={c.name} fluid />
-          </Link>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <>
+      <Carousel pause="hover" className="bg-primary mb-4">
+        {campaign?.map((c) => (
+          <Carousel.Item key={c.id}>
+            <Link
+              to="/"
+              className="d-flex align-items-center"
+              onClick={handleShowModal} // Open the modal when the link is clicked
+            >
+              <Image src={c.image} alt={c.name} fluid />
+            </Link>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+
+      <div style={{ position: "relative" }}>
+        <Modal show={showModal} onHide={handleCloseModal} centered>
+          <div className="p-4 text-center">
+            <h2>Coming Soon...</h2>
+          </div>
+        </Modal>
+      </div>
+    </>
   );
 };
 
